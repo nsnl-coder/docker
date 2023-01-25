01a646bf0ea9753efa7d0b5cbe482651af6e5a0a
nsnhatlong <nsnl.coder@gmail.com>
2023-01-25 12:09:33 -0500

    finnally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <div>
         <h1>Docker is a piece of cake</h1>
+        <h3>I finally deploy my first kuberties to the cloud</h3>
         <header>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Pages</Link>
